ab9d8338c515a12d2917bb691b009c0ab4624485
Vladislav <vladislav_chernikov94@mail.ru>
2022-12-01 22:09:05 +0700

    z27

---
+++ z27/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+int sumNumber(int numberA)
+{
+    int sum = 0;
+    while (numberA > 0)
+    {
+       int mod = numberA % 10;
+       sum = sum + mod;
+       int div = numberA / 10;
+       numberA = div;
+    }
+    return sum;
+}
+
+int summa = sumNumber(numberA);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Сумма цифр в числе {numberA} = {summa}");
+Console.ResetColor();
